c8284cc610d8f1937f7ed7ffb39b58a2e635f619
st406 <66181536+st406@users.noreply.github.com>
2021-02-08 14:41:10 +0000

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,16 +1,11 @@
 import time
 import board
-from digitalio import DigitalInOut, Direction, Pull
-import random
 import usb_hid
 from adafruit_hid.mouse import Mouse
  
 mouse = Mouse(usb_hid.devices)
-
-try:
-	while True :
-		x = random.randrange(-0.1, 0.1)
-		mouse.move(x,0)
-		time.sleep(30)
-except KeyboardInterrupt:
-    pass
+y = 5
+while True :
+	mouse.move(0,y)
+	y = y * -1
+	time.sleep(5)
